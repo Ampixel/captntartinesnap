5d316212c8475119773606a98ae085d2d163aeb8
amandine.monardo <amandine.monardo@web-atrio.com>
2023-01-23 15:38:15 +0100

    [AMM] formulaire reactif

---
+++ src/app/new-captn-face-snap/new-captn-face-snap.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
+import {CaptnFaceSnapModel} from "../models/captn-face-snap-model";
+import {map} from "rxjs/operators";
+
+@Component({
+  selector: 'app-new-captn-face-snap',
+  templateUrl: './new-captn-face-snap.component.html',
+  styleUrls: ['./new-captn-face-snap.component.scss']
+})
+export class NewCaptnFaceSnapComponent implements OnInit {
+
+  snapForm!: FormGroup;
+  captnFaceSnapPreview$!: Observable<CaptnFaceSnapModel>;
+  urlRegex!: RegExp;
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+    this.snapForm = this.formBuilder.group({
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+      location: [null]
+    }, {
+      updateOn: "blur"
+    });
+    this.captnFaceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createDate: new Date(),
+        id: 0,
+        snaps: 0
+      }))
+    );
+  }
+
+  onSubmitForm(): void {
+    console.log((this.snapForm.value));
+  }
+
+}
